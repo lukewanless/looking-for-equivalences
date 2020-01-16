98b6e0eeb338bc43ffda83dde026d72572029adb
felipessalvatore <felipessalvatore@github.com>
2020-01-16 18:30:20 -0300

    add label mapping

---
+++ src/lr/training/util.py
@@ -0,0 +1,22 @@
+
+
+def label2binary_label(df):
+    """
+    get binary label from nli labels
+    """
+    nli2binary = {"entailment": 1,
+                  "contradiction": 0,
+                  "neutral": 0}
+
+    df.loc[:, "label"] = df.label.apply(lambda x: nli2binary[x]).values
+
+
+def label2ternary_label(df):
+    """
+    get ternary label from nli labels
+    """
+    nli2ternary = {"entailment": 1,
+                   "contradiction": -1,
+                   "neutral": 0}
+
+    df.loc[:, "label"] = df.label.apply(lambda x: nli2ternary[x]).values
