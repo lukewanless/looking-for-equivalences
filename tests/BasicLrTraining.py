e39e6b44cd337d584f0edb292e3ece84ebbf6c6d
felipessalvatore <felipessalvatore@github.com>
2020-01-23 14:12:22 -0300

    add new 3 classes description

--- tests/BasicLrTraining.py
+++ tests/BasicLrTraining.py
@@ -68,8 +68,6 @@ class BasicLrTraining(unittest.TestCase):
         msg = "acc = {:.2f} baseline  = {:.2f}".format(acc, baseline)
         self.assertTrue(acc > baseline, msg)
 
-
-
     def test_save(self):
         hyperparams = {"RepresentationFunction": Tfidf,
                        "max_features": 500,
@@ -86,66 +84,6 @@ class BasicLrTraining(unittest.TestCase):
         new_scores = lr.get_score(self.df)
         msg = "problem to save results"
         self.assertTrue(all(scores == new_scores), msg)
-    #     df_new = self.df.copy()
-    #     label2ternary_label(df_new)
-    #     test = np.all(np.array([1, 0, -1]) == df_new.label.unique())
-    #     self.assertTrue(test, "not correct label")
-
-    # def test_syn_transformation(self):
-    #     df_t = get_transformed_part_by_syn(self.df, toy)
-    #     aug1 = get_augmented_data(df=self.df,
-    #                               transformation=syn2tranformation(toy),
-    #                               frac=1)
-    #     mods1 = ((aug1.premise != self.df.premise) | (
-    #         aug1.hypothesis != self.df.hypothesis)).sum()
-    #     self.assertEqual(
-    #         mods1,
-    #         df_t.shape[0],
-    #         "not all transformations being done")
-    #     self.assertEqual(aug1.shape, self.df.shape, "wrong shape")
-    #     aug2 = get_augmented_data(df=self.df,
-    #                               transformation=syn2tranformation(toy),
-    #                               frac=0.5)
-    #     mods2 = ((aug2.premise != self.df.premise) | (
-    #         aug2.hypothesis != self.df.hypothesis)).sum()
-    #     self.assertTrue(
-    #         mods2 < mods1,
-    #         "not the right number of transformations being done")
-    #     self.assertEqual(aug2.shape, self.df.shape, "wrong shape")
-
-    #     aug3 = get_augmented_data(df=self.df,
-    #                               transformation=syn2tranformation(toy),
-    #                               frac=0)
-    #     mods3 = ((aug3.premise != self.df.premise) | (
-    #         aug3.hypothesis != self.df.hypothesis)).sum()
-    #     self.assertTrue(0 <= mods3 < mods2 < mods1,
-    #                     "not the right number of transformations being done")
-    #     self.assertEqual(aug3.shape, self.df.shape, "wrong shape")
-
-    # def test_invert_transformation(self):
-    #     df_i = invert(self.df)
-    #     aug = get_augmented_data(df=self.df, transformation=invert, frac=0.5)
-    #     test1 = all(df_i.query("label!='entailment'").premise == self.df.query("label!='entailment'").hypothesis)  # noqa
-    #     test2 = all(df_i.query("label!='entailment'").hypothesis == self.df.query("label!='entailment'").premise)  # noqa
-    #     test3 = all(df_i.label == self.df.label)  # noqa
-    #     test4 = (aug.query("label!='entailment'").hypothesis != self.df.query("label!='entailment'").hypothesis).max()  # noqa
-    #     self.assertTrue(test1)
-    #     self.assertTrue(test2)
-    #     self.assertTrue(test3)
-    #     self.assertTrue(test4)
-    #     self.assertEqual(df_i.shape, self.df.shape)
-
-    # def test_entailment_internalization(self):
-    #     df_i = entailment_internalization(self.df)
-    #     aug = get_augmented_data(df=self.df, transformation=entailment_internalization, frac=0.5)
-    #     test1 = all(df_i.label == self.df.label)  # noqa
-    #     test2 = (aug.premise != self.df.premise).max()  # noqa
-    #     test3 = (aug.hypothesis != self.df.hypothesis).max()  # noqa
-    #     self.assertTrue(test1)
-    #     self.assertTrue(test2)
-    #     self.assertTrue(test3)
-    #     self.assertEqual(df_i.premise.map(lambda x: len(x)).sum(), 0)
-    #     self.assertEqual(df_i.shape, self.df.shape)
 
 
 if __name__ == '__main__':
