6ffcaf937876cf0a844d977191bd05c140fc077e
felipessalvatore <felipessalvatore@github.com>
2020-03-27 10:52:52 -0300

    add partial results roberta

--- src/script_doncovin.sh
+++ src/script_doncovin.sh
@@ -1,4 +1,4 @@
-python wordnet_syn_test_roberta.py 0.25 29 36 8
+python wordnet_syn_test_roberta.py 0.75 71 12 8
 rm data/snli/cached_*
-python wordnet_syn_test_roberta.py 0.25 143 311 8
+python wordnet_syn_test_roberta.py 0.75 524 26 8
 rm data/snli/cached_*
