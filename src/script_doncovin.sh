15f0441150a8be90b521a4a2884a71a3bab22c4a
felipessalvatore <felipessalvatore@github.com>
2020-03-28 22:54:51 -0300

    udate results

--- src/script_doncovin.sh
+++ src/script_doncovin.sh
@@ -1,4 +1,4 @@
-python wordnet_syn_test_roberta.py 0.5 108 243 8
+python wordnet_syn_test_roberta.py 0.75 1332 75 8
 rm data/snli/cached_*
-python wordnet_syn_test_roberta.py 0.5 251 342 8
+python wordnet_syn_test_roberta.py 0.75 19402 173 8
 rm data/snli/cached_*
