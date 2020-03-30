fc549543659edc00f61f53a724de41202f1c051d
felipessalvatore <felipessalvatore@github.com>
2020-03-29 22:14:51 -0300

    add new script

--- src/wordnet_syn_test_roberta.py
+++ src/wordnet_syn_test_roberta.py
@@ -19,18 +19,18 @@ def run_test(rho, dgp_seed, random_state, n_cores):
     # Variables
     folder = "snli"
     # result_folder = "results/snli/roberta_base/sin_p/"
-    result_folder = "results/snli/roberta_base/sin_h/"
+    result_folder = "results/snli/roberta_base/sin_p_h/"
     
 
-    # transformation_name = "wordnet sin tranformation p and h"
+    transformation_name = "wordnet sin tranformation p and h"
     # transformation_name = "wordnet sin tranformation p"
-    transformation_name = "wordnet sin tranformation h"
+    # transformation_name = "wordnet sin tranformation h"
 
     name = "rho_{:.2f}_dgp_seed_{}_random_state_{}".format(
         rho, dgp_seed, random_state)
     name = name.replace(".", "p")
-    # output_dir_name = "roberta_base_p_h_" + name
-    output_dir_name = "roberta_base_h_" + name
+    output_dir_name = "roberta_base_long_p_h_" + name
+    # output_dir_name = "roberta_base_h_" + name
 
     # Data
 
@@ -48,14 +48,14 @@ def run_test(rho, dgp_seed, random_state, n_cores):
     train_path_mod = "data/{}/train_p_h_syn_noun.csv".format(folder)
     dev_path_mod = "data/{}/dev_p_h_syn_noun.csv".format(folder)
 
-    # def train_trans(df): return path_base_transformation(df, train_path_mod)
-    # def dev_trans(df): return path_base_transformation(df, dev_path_mod)
+    def train_trans(df): return path_base_transformation(df, train_path_mod)
+    def dev_trans(df): return path_base_transformation(df, dev_path_mod)
 
     # def train_trans(df): return path_base_transformation_p(df, train_path_mod)
     # def dev_trans(df): return path_base_transformation_p(df, dev_path_mod)
 
-    def train_trans(df): return path_base_transformation_h(df, train_path_mod)
-    def dev_trans(df): return path_base_transformation_h(df, dev_path_mod)
+    # def train_trans(df): return path_base_transformation_h(df, train_path_mod)
+    # def dev_trans(df): return path_base_transformation_h(df, dev_path_mod)
 
     print("transform dev")
     dev_t = dev_trans(dev_o)
@@ -73,9 +73,9 @@ def run_test(rho, dgp_seed, random_state, n_cores):
                    "weight_decay": 0.0,
                    "adam_epsilon": 1e-8,
                    "max_grad_norm": 1.0,
-                   "max_steps": 1505,
+                   "max_steps": -1,
                    "warmup_steps": 0,
-                   "save_steps": 1500,
+                   "save_steps": 8580,
                    "no_cuda": False,
                    "n_gpu": 1,
                    "data_set_name": folder,
