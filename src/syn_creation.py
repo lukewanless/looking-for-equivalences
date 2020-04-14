135a3ac89d1e540ca72ae0e20bb1c86797a5876b
felipessalvatore <felipessalvatore@github.com>
2020-04-13 23:08:24 -0300

    add param to train sample

--- src/syn_creation.py
+++ src/syn_creation.py
@@ -27,6 +27,7 @@ folder = args.folder
 n_cores = args.n_cores
 
 train_path = "data/{}/train.csv".format(folder)
+train_sample_path = "data/{}/train_sample.csv".format(folder)
 dev_path = "data/{}/dev.csv".format(folder)
 test_path = "data/{}/test.csv".format(folder)
 
@@ -34,24 +35,31 @@ veto_path = "data/{}/syn_veto.csv".format(folder)
 syn_path = "data/{}/syn_noun.csv".format(folder)
 
 output_train_path = "data/{}/train_p_h_syn_noun.csv".format(folder)
+output_train_sample_path = "data/{}/train_sample_p_h_syn_noun.csv".format(
+    folder)
 output_dev_path = "data/{}/dev_p_h_syn_noun.csv".format(folder)
 output_test_path = "data/{}/test_p_h_syn_noun.csv".format(folder)
 
 
 train = pd.read_csv(train_path)
+train_sample = pd.read_csv(train_sample_path)
 dev = pd.read_csv(dev_path)
 test = pd.read_csv(test_path)
 
 # cleaning
 train = filter_df_by_label(train.dropna()).reset_index(drop=True)
+train_sample = filter_df_by_label(train_sample.dropna()).reset_index(drop=True)
+
 dev = filter_df_by_label(dev.dropna()).reset_index(drop=True)
 test = filter_df_by_label(test.dropna()).reset_index(drop=True)
 
 pre_process_nli_df(train)
+pre_process_nli_df(train_sample)
 pre_process_nli_df(dev)
 pre_process_nli_df(test)
 
 print("train.shape", train.shape)
+print("train_sample.shape", train_sample.shape)
 print("dev.shape", dev.shape)
 print("test.shape", test.shape)
 
@@ -94,6 +102,12 @@ trans_time = time() - init
 print("applying trans to train: {:.4f} minutes".format(trans_time / 60))
 
 init = time()
+train_sample_t = parallelize(df=train_sample, func=trans, n_cores=n_cores)
+trans_time = time() - init
+print("applying trans to train_sample: {:.4f} minutes".format(trans_time / 60))
+
+
+init = time()
 dev_t = parallelize(df=dev, func=trans, n_cores=n_cores)
 trans_time = time() - init
 print("applying trans to dev: {:.4f} minutes".format(trans_time / 60))
@@ -104,6 +118,6 @@ trans_time = time() - init
 print("applying trans to test: {:.4f} minutes".format(trans_time / 60))
 
 train_t.to_csv(output_train_path, index=False)
+train_sample_t.to_csv(output_train_sample_path, index=False)
 dev_t.to_csv(output_dev_path, index=False)
 test_t.to_csv(output_test_path, index=False)
-
